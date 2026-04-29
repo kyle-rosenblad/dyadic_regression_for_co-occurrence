21f34ad161955cee8575435d85224f2e5f8e4388
kyle-rosenblad <kyle.c.rosenblad@gmail.com>
2026-04-28 18:43:12 -0700

    initial setup

---
+++ compnet_ms_sbc/sbc_ame_bin1.R
@@ -0,0 +1,215 @@
+### This script simulates data from a rank-2 compnet model
+# and then analyzes the data with compnet to confirm that
+# compnet can recover correct parameter values.
+
+set.seed(237)
+
+options(scipen=999) # preserve plenty of numerical precision
+library(compnet)
+library(rstan)
+library(mvtnorm)
+library(foreach)
+library(doParallel)
+registerDoParallel(16)
+
+# Set up data structure
+dyads <- t(utils::combn(c(1:20),2))
+d <- as.data.frame(dyads)
+names(d) <- c("spAid", "spBid")
+
+# For simplicity, use 100 trials for each dyad
+d$either <- 100
+spp <- sort(unique(c(d$spAid, d$spBid)))
+
+# Randomly draw model parameter values
+alpha <- 1.2
+sigma <- 0.8
+trait1_trait2_corr <- 0.3
+beta_trait1_sp <- -0.5
+beta_trait1_dist <- 1.6
+beta_trait2_sp <- 0.7
+beta_trait2_dist <- -2.2
+
+# The parameters below are not uniquely identifiable
+# due to rotational invariance, reflectional invariance,
+# label-switching (when model rank>1), and redundant
+# scale factors between lambda_diag and multi_effects_sigma.
+# Instead of fitting all these parameters, compnet just z-scores
+# each latent factor across species and treats each latent factor
+# as independent. My intent is to show that even when we specify all
+# these nonidentifiable parameters in the data-generating process, the key quantity they
+# produce in the linear predictor--i.e., the product of U, the matrix of latent
+# factor values, lambda, and U-transpose--is still identified by my simpler
+# model with orthogonal, z-scored latent factors. This approach deals with the 
+# rotational invariance issue and the redundant scale factors.
+# ame_binomial.stan also fixes the first element of each latent factor to be positive,
+# to deal with reflectional invariance, and sorts lambda_diag, to deal with
+# label-switching.
+multi_effects_corr <- -0.4
+multi_effects_sigma <- c(0.5, 0.4)
+multi_effects_sigma <- diag(multi_effects_sigma) %*% 
+  matrix(c(1, multi_effects_corr, multi_effects_corr, 1), nrow=2) %*% 
+  diag(multi_effects_sigma)
+lambda_diag <- c(-1, 1.2)
+
+
+# Simulate one data set for a dummy model (see below)
+traits <- rmvnorm(n=length(spp), mean=c(0,0), sigma=matrix(c(1, trait1_trait2_corr, trait1_trait2_corr, 1), nrow=2))
+latfacs <- rmvnorm(n=length(spp), mean=c(0,0), sigma=multi_effects_sigma)
+spdata <- data.frame(spid=spp,
+                     a=rnorm(n=length(spp), mean=0, sd=sigma),
+                     latfac1=latfacs[,1],
+                     latfac2=latfacs[,2],
+                     trait1=scale(traits[,1]),
+                     trait2=scale(traits[,2]))
+for(i in 1:nrow(d)){
+  d[i, "trait1_A"] <- spdata[spdata$spid==d[i,"spAid"], "trait1"]
+  d[i, "trait1_B"] <- spdata[spdata$spid==d[i,"spBid"], "trait1"]
+  d[i, "trait2_A"] <- spdata[spdata$spid==d[i,"spAid"], "trait2"]
+  d[i, "trait2_B"] <- spdata[spdata$spid==d[i,"spBid"], "trait2"]
+  d[i, "avalA"] <- spdata[spdata$spid==d[i,"spAid"], "a"]
+  d[i, "avalB"] <- spdata[spdata$spid==d[i,"spBid"], "a"]
+  d[i, "latfac1A"] <- spdata[spdata$spid==d[i,"spAid"], "latfac1"]
+  d[i, "latfac1B"] <- spdata[spdata$spid==d[i,"spBid"], "latfac1"]
+  d[i, "latfac2A"] <- spdata[spdata$spid==d[i,"spAid"], "latfac2"]
+  d[i, "latfac2B"] <- spdata[spdata$spid==d[i,"spBid"], "latfac2"]
+}
+
+for(i in 1:nrow(d)){
+  d[i, "latfacterm"] <- (as.matrix(d[i,c("latfac1A", "latfac2A")]))%*%diag(lambda_diag, nrow=2)%*%(t(d[i,c("latfac1B", "latfac2B")]))
+}
+
+d$trait1_dist <- abs(d$trait1_A-d$trait1_B)
+d$trait2_dist <- abs(d$trait2_A-d$trait2_B)
+d$pboth <- expit(alpha + d$avalA + d$avalB +
+                   beta_trait1_sp*d$trait1_A +
+                   beta_trait1_sp*d$trait1_B +
+                   beta_trait1_dist*d$trait1_dist+
+                   beta_trait2_sp*d$trait2_A +
+                   beta_trait2_sp*d$trait2_B +
+                   beta_trait2_dist*d$trait2_dist+
+                   d$latfacterm)
+
+d$both <- rbinom(n=nrow(d), size=d$either, prob=d$pboth)
+
+# Set up dummy model so models can be fit without recompiling in the foreach loop
+datalist <- list(
+  n_nodes=length(spp),
+  N=nrow(d),
+  Xdy_cols=2,
+  Xsp_cols=2,
+  spAid=d$spAid,
+  spBid=d$spBid,
+  Xdy=d[c("trait1_dist", "trait2_dist")],
+  XA=d[c("trait1_A", "trait2_A")],
+  XB=d[c("trait1_B", "trait2_B")],
+  K=2,
+  both=d$both,
+  either=d$either,
+  prior_intercept_scale=5,
+  prior_betas_scale=5,
+  prior_sigma_addeff_rate=1,
+  prior_lambda_scale=5)
+stanmod_dummy <- stan(file="ame_binomial.stan",
+                      data=datalist,
+                      cores=1,
+                      chains=1,
+                      warmup=1,
+                      iter=2,
+                      verbose=F)
+
+
+
+# Simulate and analyze 16 replicate data sets
+
+sbc_mods_and_data <- foreach(u=1:16) %dopar% {
+  library(compnet)
+  library(rstan)
+  library(mvtnorm)
+  dyads <- t(utils::combn(c(1:20),2))
+  d <- as.data.frame(dyads)
+  names(d) <- c("spAid", "spBid")
+  
+  d$either <- 100
+  spp <- sort(unique(c(d$spAid, d$spBid)))
+  
+  traits <- rmvnorm(n=length(spp), mean=c(0,0), sigma=matrix(c(1, trait1_trait2_corr, trait1_trait2_corr, 1), nrow=2))
+  latfacs <- rmvnorm(n=length(spp), mean=c(0,0), sigma=multi_effects_sigma)
+  spdata <- data.frame(spid=spp,
+                       a=rnorm(n=length(spp), mean=0, sd=sigma),
+                       latfac1=latfacs[,1],
+                       latfac2=latfacs[,2],
+                       trait1=scale(traits[,1]),
+                       trait2=scale(traits[,2]))
+  for(i in 1:nrow(d)){
+    d[i, "trait1_A"] <- spdata[spdata$spid==d[i,"spAid"], "trait1"]
+    d[i, "trait1_B"] <- spdata[spdata$spid==d[i,"spBid"], "trait1"]
+    d[i, "trait2_A"] <- spdata[spdata$spid==d[i,"spAid"], "trait2"]
+    d[i, "trait2_B"] <- spdata[spdata$spid==d[i,"spBid"], "trait2"]
+    d[i, "avalA"] <- spdata[spdata$spid==d[i,"spAid"], "a"]
+    d[i, "avalB"] <- spdata[spdata$spid==d[i,"spBid"], "a"]
+    d[i, "latfac1A"] <- spdata[spdata$spid==d[i,"spAid"], "latfac1"]
+    d[i, "latfac1B"] <- spdata[spdata$spid==d[i,"spBid"], "latfac1"]
+    d[i, "latfac2A"] <- spdata[spdata$spid==d[i,"spAid"], "latfac2"]
+    d[i, "latfac2B"] <- spdata[spdata$spid==d[i,"spBid"], "latfac2"]
+  }
+  
+  for(i in 1:nrow(d)){
+    d[i, "latfacterm"] <- (as.matrix(d[i,c("latfac1A", "latfac2A")]))%*%diag(lambda_diag, nrow=2)%*%(t(d[i,c("latfac1B", "latfac2B")]))
+  }
+  
+  d$trait1_dist <- abs(d$trait1_A-d$trait1_B)
+  d$trait2_dist <- abs(d$trait2_A-d$trait2_B)
+  d$pboth <- expit(alpha + d$avalA + d$avalB +
+                     beta_trait1_sp*d$trait1_A +
+                     beta_trait1_sp*d$trait1_B +
+                     beta_trait1_dist*d$trait1_dist+
+                     beta_trait2_sp*d$trait2_A +
+                     beta_trait2_sp*d$trait2_B +
+                     beta_trait2_dist*d$trait2_dist+
+                     d$latfacterm)
+  
+  d$both <- rbinom(n=nrow(d), size=d$either, prob=d$pboth)
+  
+  datalist <- list(
+    n_nodes=length(spp),
+    N=nrow(d),
+    Xdy_cols=2,
+    Xsp_cols=2,
+    spAid=d$spAid,
+    spBid=d$spBid,
+    Xdy=d[c("trait1_dist", "trait2_dist")],
+    XA=d[c("trait1_A", "trait2_A")],
+    XB=d[c("trait1_B", "trait2_B")],
+    K=2,
+    both=d$both,
+    either=d$either,
+    prior_intercept_scale=5,
+    prior_betas_scale=5,
+    prior_sigma_addeff_rate=1,
+    prior_lambda_scale=5)
+  
+  stanmod <- stan(file="ame_binomial.stan",
+                  fit=stanmod_dummy,
+                  pars=c("intercept", # Marginalize over the individual values of each latent factor ("U"), since these are known to be nonidentifiable due to reflectional and rotational invariance, as well as potential label-switching among multiple latent factor dimensions when K>1. The Stan code has tools designed to deal with this, but only for computational efficiency reasons; it's not a problem for inference.
+                         "beta_dy",
+                         "beta_sp",
+                         "sigma_species_randint",
+                         "species_randint",
+                         "xb",
+                         "pboth",
+                         "latfacterm", # In contrast to individual species' U values, ULU values (sensu Hoff and colleagues) need to be identifiable, so we'll include them to make sure they sample well.
+                         "lp__"),
+                  data=datalist,
+                  cores=1,
+                  chains=1,
+                  warmup=1000,
+                  iter=3000,
+                  verbose=F,
+                  control=list(adapt_delta=0.99))
+  truepars <- list(alpha, sigma, beta_trait1_sp, beta_trait1_dist, beta_trait2_sp, beta_trait2_dist, latfacs)
+  tmpout <- list(stanmod, d, datalist, truepars)
+  tmpout
+}
+saveRDS(sbc_mods_and_data, "sbc_ame_bin1_out.RDS")
+
---
+++ compnet_ms_sbc/sbc_ame_bin4_processing.R
@@ -0,0 +1,138 @@
+options(scipen=999) # preserve plenty of numerical precision
+library(compnet)
+library(rstan)
+library(foreach)
+library(doParallel)
+registerDoParallel(cores=8)
+
+
+library(rstan)
+md <- readRDS("sbc_ame_bin4_out.RDS")
+intercept <- md[[1]][[4]][[1]]
+sigma <- md[[1]][[4]][[2]]
+beta_trait1_sp <- md[[1]][[4]][[3]]
+beta_trait1_dist <- md[[1]][[4]][[4]]
+beta_trait2_sp <- md[[1]][[4]][[5]]
+beta_trait2_dist <- md[[1]][[4]][[6]]
+for(v in 1:16){
+  srmpars <- data.frame(rep=c(),
+                        intercept=c(),
+                        sigma=c(),
+                        beta_trait1_sp=c(),
+                        beta_trait1_dist=c(),
+                        beta_trait2_sp=c(),
+                        beta_trait2_dist=c())
+  for(i in 1:length(md)){
+    tmpmod <- md[[i]][[1]]
+    tmpdraws <- extract(tmpmod)
+    srmpars_tmp <- data.frame(rep=i,
+                              intercept=tmpdraws$intercept,
+                              sigma=tmpdraws$sigma_species_randint,
+                              beta_trait1_sp=tmpdraws$beta_sp[,1],
+                              beta_trait1_dist=tmpdraws$beta_dy[,1],
+                              beta_trait2_sp=tmpdraws$beta_sp[,2],
+                              beta_trait2_dist=tmpdraws$beta_dy[,2])
+    srmpars <- rbind(srmpars, srmpars_tmp)
+  }
+}
+library(ggplot2)
+intercept_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=intercept, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=intercept), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=intercept)+
+  geom_vline(xintercept=mean(srmpars$intercept), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Intercept")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+sigma_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=sigma, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=sigma), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=sigma)+
+  geom_vline(xintercept=mean(srmpars$sigma), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Additive Species Effect SD")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+beta_trait1_sp_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=beta_trait1_sp, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=beta_trait1_sp), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=beta_trait1_sp)+
+  geom_vline(xintercept=mean(srmpars$beta_trait1_sp), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Species Effect, Trait 1")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+beta_trait1_dist_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=beta_trait1_dist, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=beta_trait1_dist), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=beta_trait1_dist)+
+  geom_vline(xintercept=mean(srmpars$beta_trait1_dist), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Distance Effect, Trait 1")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+beta_trait2_sp_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=beta_trait2_sp, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=beta_trait2_sp), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=beta_trait2_sp)+
+  geom_vline(xintercept=mean(srmpars$beta_trait2_sp), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Species Effect, Trait 2")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+beta_trait2_dist_plot <- ggplot(srmpars)+
+  geom_hline(yintercept=0)+
+  geom_density(aes(x=beta_trait2_dist, group=rep), color=alpha("blue", 0.1))+
+  geom_density(aes(x=beta_trait2_dist), color="blue", linewidth=1.5)+
+  geom_vline(xintercept=beta_trait2_dist)+
+  geom_vline(xintercept=mean(srmpars$beta_trait2_dist), color="blue", linewidth=1.5, linetype="dashed")+
+  xlab("Estimated Distance Effect, Trait 2")+
+  ylab("Density")+
+  theme_bw()+
+  theme(aspect.ratio=1)
+
+latfacresults <- list()
+for(i in 1:length(md)){
+  tmpmod <- md[[i]][[1]]
+  tmpdraws <- extract(tmpmod, pars="latfacterm")
+  latfacresults[[i]] <- data.frame(lfmean=apply(tmpdraws$latfacterm, c(2), mean),
+                                   lflow=apply(tmpdraws$latfacterm, c(2), quantile, 0.025),
+                                   lfhigh=apply(tmpdraws$latfacterm, c(2), quantile, 0.975))
+}
+for(i in 1:length(md)){
+  dtmp <- cbind(md[[i]][[2]], latfacresults[[i]])
+  dtmp$rep <- i
+  if(i==1){
+    d <- dtmp
+  }
+  if(i>1){
+    d <- rbind(d, dtmp)
+  }
+}
+
+latfacterm_plot <- ggplot(d, aes(x=latfacterm, xend=latfacterm, y=lflow, yend=lfhigh))+
+  geom_hline(yintercept=0)+
+  geom_vline(xintercept=0)+
+  geom_abline(slope=1,intercept=0)+
+  geom_segment(color="blue", alpha=0.1)+
+  xlab("True Latent Factor Term")+
+  ylab("Estimated Latent Factor Term")+
+  theme_bw()+
+  theme(aspect.ratio = 1)
+
+library(patchwork)
+intercept_plot+sigma_plot+beta_trait1_sp_plot+beta_trait1_dist_plot+
+  beta_trait2_sp_plot+beta_trait2_dist_plot+latfacterm_plot
+ggsave("../output_files/sbc_ame_bin4_plot.png", height=10, width=10)
+
